d96b56ee7f2e8e86f725a7e6810cb46de4dbcf80
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-12 16:36:35 +0100

    feat: footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Navbar from "./components/Navbar/Navbar.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
