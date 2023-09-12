fc09ca9ff5659ce48e1abeddb4e9507010c87866
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-12 15:45:16 +0100

    feat: navbar

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from "vue";
+import SearchBarIcon from "@/components/icons/searchBarIcon.vue";
 const body = document.querySelector("body");
 body.classList.remove("overflow-hidden");
 const showMobile = ref(false);
@@ -16,7 +17,7 @@ const toogleMobileMenu = () => {
 // const landingUrl = import.meta.env.VITE_LANDING_PAGE;
 </script>
 <template>
-  <div class="auth_container">
+  <div class="max-w-[90%] px-[15px] mx-auto">
     <header class="py-8">
       <div class="lg:px-[15px] mx-auto flex items-center justify-between font-Satoshi700">
         <div class="flex items-center justify-between space-x-4 lg:space-x-6">
@@ -28,30 +29,51 @@ const toogleMobileMenu = () => {
             />
           </router-link>
         </div>
-        <div class="hidden md:flex items-center gap-12">
-          <ul class="hidden lg:flex gap-14 text-sm"></ul>
-          <div role="button" class="flex gap-2">
-            <!-- <a :href="landingUrl" class="font-Satoshi400 text-center text-brand">
-              back to website
-            </a> -->
-            <!-- <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="21"
-              viewBox="0 0 20 21"
-              fill="none"
-            >
-              <path
-                d="M3.75 10.5391L16.25 10.5391M16.25 10.5391L10.625 4.91406M16.25 10.5391L10.625 16.1641"
-                stroke="#2F929C"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg> -->
-          </div>
+        <div class="hidden xl:flex items-center gap-12">
+          <ul class="hidden xl:flex gap-14 text-sm">
+            <li>
+              <div class="w-full relative hidden lg:flex">
+                <SearchBarIcon class="absolute left-3 top-3" /><input
+                  class="w-full font-light font-Satoshi400 text-[14px] p-3 pl-9 pr-24 border-[#F0F0F0] border-[1px] opacity-[0.8029] rounded-[7px] text-sm"
+                  placeholder=" Search here.."
+                  type="text"
+                /><button
+                  class="absolute right-0 top-3 px-[21px] border-l-[#F0F0F0] border-l-[1px]"
+                >
+                  Find job
+                </button>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div class="hidden">
+        <div class="hidden xl:flex">
+          <ul class="flex flex-row font-Satoshi500 items-center gap-5">
+            <li>
+              <router-link to="/" class="p-[10px] pr-0 flex justify-start rounded-[5px]">
+                <span class="place-self-center text-[16px] leading-[21.6px]"
+                  >For Business
+                </span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/" class="p-[10px] pr-0 flex justify-start rounded-[5px]">
+                <span class="place-self-center text-[16px] leading-[21.6px]">Learn </span>
+              </router-link>
+            </li>
+            <li>
+              <router-link to="/" class="p-[10px] pr-0 flex justify-start rounded-[5px]">
+                <span class="place-self-center text-[16px] leading-[21.6px]"
+                  >Hire Talent
+                </span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+        <div class="hidden lg:flex gap-4">
+          <button class="btn-light">Sign in</button>
+          <button class="btn-brand">Start free trial</button>
+        </div>
+        <div class="lg:hidden">
           <button @click="toogleMobileMenu">
             <svg
               v-if="!showMobile"
---
+++ src/components/icons/searchBarIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="17"
+      height="17"
+      viewBox="0 0 17 17"
+      fill="none"
+    >
+      <path
+        d="M14.4082 7.97219C14.4082 11.576 11.4731 14.503 7.8457 14.503C4.21826 14.503 1.2832 11.576 1.2832 7.97219C1.2832 4.36841 4.21826 1.44141 7.8457 1.44141C11.4731 1.44141 14.4082 4.36841 14.4082 7.97219Z"
+        stroke="black"
+        stroke-opacity="0.5"
+        stroke-width="1.5"
+      />
+      <path
+        d="M12.5078 12.6133L15.8102 16.3711"
+        stroke="black"
+        stroke-opacity="0.5"
+        stroke-width="1.5"
+      />
+    </svg>
+  </div>
+</template>
