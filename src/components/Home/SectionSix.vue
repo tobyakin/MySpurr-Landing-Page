f23a671c8eeb2d68b903c94116708bf5124f007e
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-14 11:22:55 +0100

    feat: section six

---
+++ src/components/Home/SectionSix.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container py-40">
+    <div class="w-full">
+      <iframe
+        class="w-full lg:h-[80vh] h-[50vh] rounded-[14px]"
+        width="auto"
+        height="auto"
+        src="https://www.youtube.com/embed/1f-vEXh_XaI"
+        title="Celebrate your passion"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </div>
+</template>
+
+<style></style>
