212ba560d50b72049cbef16625e42ed3fce52905
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-17 21:52:14 +0100

    feat: blog page

---
+++ src/views/Blog/BlogPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from "@/components/Navbar/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <div></div>
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
