33edf59af2362bc3d30efd801186565ed87f937a
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-19 15:40:12 +0100

    Docs : about us

--- src/components/About/SectionOne.vue
+++ src/components/About/SectionOne.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="justify-center container items-center py-20">
     <div class="mb-10">
-      <h4 class="font-EBGaramond500 lg:text-[64px] text-[30px] text-center text-[#007582]">
+      <h4
+        class="font-EBGaramond500 lg:text-[64px] text-[30px] text-center text-[#007582]"
+      >
         About us
       </h4>
       <p
-        class="text-[#011B1F] text-[79.379px] font-EBGaramond500 mt-8 leading-[86.941px] text-center"
+        class="text-[#011B1F] lg:text-[79.379px] text-[50px] font-EBGaramond500 mt-8 leading-[45px] lg:leading-[86.941px] text-center"
       >
         Connecting Creativity, <br class="md:block hidden" />
         Fueling Dreams
@@ -13,22 +15,28 @@
     </div>
     <img src="@/assets/img/aboutbg.webp" class="max-h-full" alt="" />
     <div class="container">
-      <div class="flex items-center flex-row justify-between my-8 gap-6">
-        <div class="flex items-center flex-row gap-6">
+      <div
+        class="flex items-center lg:flex-row flex-col-reverse justify-between my-8 gap-6"
+      >
+        <div class="flex lg:flex-row flex-col items-center gap-6">
           <img
             src="@/assets/img/BrandCEO.webp"
             class="w-[103.116px] h-[103.116px] rounded-full"
             alt=""
           />
           <div>
-            <h5 class="text-[24.224px] font-EBGaramond500 text-[#011B1F]">Tobi Akinyele</h5>
+            <h5 class="text-[24.224px] font-EBGaramond500 text-[#011B1F]">
+              Tobi Akinyele
+            </h5>
             <p class="font-Satoshi400 text-[12.828px] text-[#011B1F] leading-[20.781px]">
               Founder MySpurr
             </p>
           </div>
         </div>
         <div>
-          <h4 class="text-[#007582] font-Satoshi500 text-[52.263px]">
+          <h4
+            class="text-[#007582] font-Satoshi500 text-[30px] leading-[35px] lg:text-left text-center lg:text-[52.263px]"
+          >
             Inspiration from <br class="md:block hidden" />
             Passion
           </h4>
@@ -36,17 +44,19 @@
       </div>
       <div class="text-[#000000B2] font-Satoshi400 text-[18px] leading-[32px]">
         <p>
-          In today's economy, there is a growing demand for creative professionals in industries
-          such as advertising, media, design, and technology. However, many talented individuals in
-          Africa still struggle to find job opportunities that match their skills and experience,
-          and may not have access to the resources they need to forge sustainable career paths.
+          In today's economy, there is a growing demand for creative professionals in
+          industries such as advertising, media, design, and technology. However, many
+          talented individuals in Africa still struggle to find job opportunities that
+          match their skills and experience, and may not have access to the resources they
+          need to forge sustainable career paths.
         </p>
         <p class="mt-4">
-          MySpurr was born from a personal mission to help creative talents live out their best
-          lives by doing what they love. Our journey has begun and it’s with a simple idea:
+          MySpurr was born from a personal mission to help creative talents live out their
+          best lives by doing what they love. Our journey has begun and it’s with a simple
+          idea:
           <span class="text-[#007582] font-Satoshi500"
-            >Provide a platform where creativity could thrive, talents could flourish, and dreams
-            could take flight.</span
+            >Provide a platform where creativity could thrive, talents could flourish, and
+            dreams could take flight.</span
           >
         </p>
       </div>
