35bb9e0d3c639b230c7c3fffe879510025e2745e
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-19 21:02:31 +0100

    feat : internship

---
+++ src/views/InternshipView.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from "@/components/Navbar/Navbar.vue";
+import Footer from "@/components/Footer.vue";
+import SectionOne from "@/components/Internship/SectionOne.vue";
+import TrustedByBrands from "@/components/Bander/TrustedByBrands.vue";
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <SectionOne />
+    <TrustedByBrands />
+    <Footer />
+  </div>
+</template>
+
+<style scoped></style>
