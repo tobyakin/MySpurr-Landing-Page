a88c362316cb90427bffbf90f9411a7e806f61cc
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-21 22:52:19 +0100

    feat: scroll behavior

--- src/router/index.js
+++ src/router/index.js
@@ -68,7 +68,10 @@ const router = createRouter({
       name: 'talent-profile',
       component: TalentProfile
     }
-  ]
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 }
+  }
 })
 
 export default router
