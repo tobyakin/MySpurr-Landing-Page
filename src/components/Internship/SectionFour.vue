f52cb2def7fa4ea1342fdc137e3d89c84f167254
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-22 01:36:49 +0100

    Docs: updated

--- src/components/Internship/SectionFour.vue
+++ src/components/Internship/SectionFour.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="container lg:py-[145px] !lg:px-0 h-screen lg:mb-16">
+  <section class="container lg:py-[145px] !lg:px-0 lg:mb-16">
     <div class="grid lg:grid-cols-2 grid-cols-1">
       <div class="">
         <img class="h-[100%] w-[100%]" src="@/assets/img/biglogo.png" alt="" />
@@ -12,10 +12,14 @@
           <h4 class="font-Satoshi500 text-[#949939] text-[23.559px] mb-3">
             FOR BUSINESSES
           </h4>
-          <h4 class="text-[#007582] text-[72px] font-EBGaramond500 leading-[67.5px]">
+          <h4
+            class="text-[#007582] text-[45px] lg:text-[72px] font-EBGaramond500 leading-[40px] lg:leading-[67.5px]"
+          >
             Host Interns with MySpurr
           </h4>
-          <p class="text-[#000000B2] font-Satoshi400 text-[24px] leading-[43px]">
+          <p
+            class="text-[#000000B2] font-Satoshi400 text-[18px] lg:text-[24px] leading-[30px] lg:leading-[43px]"
+          >
             Are you a business looking to tap into the fresh talent pool? MySpurr
             Internships provides a platform to connect with bright and motivated interns.
             Discover the benefits of hosting interns and shaping the future of your
