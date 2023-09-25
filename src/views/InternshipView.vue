11eaaa5c7c49ee95ff4a8f23af1faf9e76e5a6c3
kufre-abasi <kufreabasibassey3@gmail.com>
2023-09-25 13:38:54 +0100

    feat: section five

--- src/views/InternshipView.vue
+++ src/views/InternshipView.vue
@@ -6,6 +6,7 @@ import TrustedByBrands from "@/components/Bander/TrustedByBrands.vue";
 import SectionTwo from "@/components/Internship/SectionTwo.vue";
 import SectionThree from "@/components/Internship/SectionThree.vue";
 import SectionFour from "@/components/Internship/SectionFour.vue";
+import SectionFive from "../components/Internship/SectionFive.vue";
 </script>
 
 <template>
@@ -16,6 +17,7 @@ import SectionFour from "@/components/Internship/SectionFour.vue";
     <SectionTwo />
     <SectionThree />
     <SectionFour />
+    <SectionFive />
     <Footer />
   </div>
 </template>
