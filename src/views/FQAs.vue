b7d1619c15d6232e03e15fdabf84075b1f06a303
kufre-abasi <kufreabasibassey3@gmail.com>
2023-12-09 13:12:48 +0100

    docs: updated

--- src/views/FQAs.vue
+++ src/views/FQAs.vue
@@ -32,14 +32,18 @@ useHead({
   ],
 });
 const toogleFaq = (index) => {
-  store.faqs[index].showContent = !store.faqs[index].showContent;
+  if (tab.value == "ALL") {
+    store.faqs[index].showContent = !store.faqs[index].showContent;
+  } else {
+    filteredTab.value[index].showContent = !filteredTab.value[index].showContent;
+  }
 };
 
 const filterTab = (category) => {
   tab.value = category;
   filteredTab.value = [];
   if (category != "ALL") {
-    filteredTab.value = store.blogPost.filter((item) => item.blog_category == category);
+    filteredTab.value = store.faqs.filter((item) => item.fags_category == category);
   }
 };
 </script>
@@ -75,9 +79,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab('User Accounts')"
+              @click="filterTab('user-accounts')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == 'User Accounts',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'user-accounts',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -87,9 +91,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab('Creative Talent')"
+              @click="filterTab('creative-talents')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == 'Creative Talent',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'creative-talents',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -99,9 +103,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab('Business')"
+              @click="filterTab('business')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == 'Business',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'business',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -111,9 +115,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab('Subscription and Payments')"
+              @click="filterTab('subscription-and-payments')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == 'Subscription and Payments',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'subscription-and-payments',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -123,9 +127,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab('Technical Support')"
+              @click="filterTab('technical-support')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == 'Technical Support',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'technical-support',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -135,9 +139,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab('Privacy')"
+              @click="filterTab('privacy')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == 'Privacy',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'privacy',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -147,9 +151,9 @@ const filterTab = (category) => {
           <li>
             <a
               href="javascript:void(0)"
-              @click="filterTab(' Community Guidelines')"
+              @click="filterTab('community-guidelines')"
               :class="{
-                'bg-[#43D0DF] text-[#244034] ': tab == ' Community Guidelines',
+                'bg-[#43D0DF] text-[#244034] ': tab == 'community-guidelines',
               }"
               class="px-4 py-2 hover:bg-[#43D0DF] hover:text-[#244034] text-[#00000080] rounded-[12px]"
             >
@@ -160,9 +164,65 @@ const filterTab = (category) => {
       </div>
 
       <div class="py-16">
-        <ul class="flex flex-col font-Satoshi500 text-[#404041]">
+        <ul
+          v-if="store.faqs.length && tab == 'ALL'"
+          class="flex flex-col font-Satoshi500 text-[#404041]"
+        >
           <li
             v-for="(faq, index) in store.faqs"
+            :key="faq"
+            class="border-b-[#00000014] border-b-[1px]"
+          >
+            <div
+              @click="toogleFaq(index)"
+              class="py-8 px-6 flex justify-between items-center cursor-pointer text-xl"
+            >
+              <div>{{ faq.name }}</div>
+              <div>
+                <div
+                  v-if="!faq.showContent"
+                  class="w-6 transition delay-100 text-[24px] ease-in-out h-6"
+                >
+                  +
+                </div>
+                <div
+                  v-if="faq.showContent"
+                  class="w-6 transition delay-100 text-[24px] ease-in-out h-6"
+                >
+                  -
+                </div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  :class="{ 'rotate-180': faq.showContent }"
+                  class="w-6 transition hidden delay-100 ease-in-out h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div v-if="faq.showContent" class="p-4">
+              <div v-html="faq.content" class="text-[16px] font-Satoshi400 mb-4"></div>
+              <ul>
+                <li class="lg:flex mb-4" v-for="list in faq.sub_content" :key="list">
+                  <h3 class="font-Satoshi700 lg:mr-2">{{ list.heading }}</h3>
+                  <p class="font-Satoshi400">{{ list.content }}</p>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+
+        <ul v-else class="flex flex-col font-Satoshi500 text-[#404041]">
+          <li
+            v-for="(faq, index) in filteredTab"
             :key="faq"
             class="border-b-[#00000014] border-b-[1px]"
           >
