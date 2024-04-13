5a4fbf0b00bd4b947a971f16c223aef1c0ebe8ca
kufre-abasi <kufreabasibassey3@gmail.com>
2024-04-13 16:32:23 +0100

    docs: modifications on porfolio

--- src/components/CaseStudy/CaseStudyCard.vue
+++ src/components/CaseStudy/CaseStudyCard.vue
@@ -2,20 +2,26 @@
   <div class="w-full font-Satoshi400" :class="{ 'md:w-[45%]': two, 'md:w-[31%]': three }">
     <div class="">
       <router-link :to="{ name: 'single-portfolio', params: { id: blog.id } }">
-        <img loading="lazy" class="w-full h-auto max-h-[283.472px] rounded-lg" :src="image" />
+        <img
+          loading="lazy"
+          class="w-full bg-[#EFF6F3] object-cover h-[188.32px] rounded-lg"
+          :src="image"
+        />
       </router-link>
       <div>
-        <div class="flex items-center justify-between gap-2 py-4 mt-2">
+        <div class="flex items-center justify-between gap-2 py-2">
           <div>
             <h3 class="font-Satoshi500 text-[12.778px] leading-[16.327px] text-[#000000]">
               {{ heading }}
             </h3>
-            <p class="font-Satoshi400 text-[10.648px] leading-[16.327px] text-[#00000066]">
+            <p
+              class="font-Satoshi400 text-[10.648px] leading-[16.327px] text-[#00000066]"
+            >
               {{ blog?.user?.first_name }}
             </p>
           </div>
 
-          <div class="flex items-center gap-2">
+          <!-- <div class="flex items-center gap-2">
             <div class="flex items-center gap-2 font-Satoshi500">
               <span class="text-[14.048px] text-[#00000066] font-Satoshi400">1234</span>
               <EyeIcon class="h-[20.078px] w-[20.078px]" />
@@ -24,7 +30,7 @@
               <span class="text-[14.048px] text-[#00000066] font-Satoshi400">15</span
               ><CommentIcon class="h-[15.358px] w-[15.358px]" />
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
       <!-- <div class="flex justify-between text-sm mt-6">
@@ -52,10 +58,10 @@
 </template>
 
 <script setup>
-import dayjs from 'dayjs'
-import EyeIcon from '@/components/icons/eyeIcon.vue'
-import CommentIcon from '@/components/icons/commentIcon.vue'
-import ShareIcon from '@/components/icons/shareIcon.vue'
+import dayjs from "dayjs";
+import EyeIcon from "@/components/icons/eyeIcon.vue";
+import CommentIcon from "@/components/icons/commentIcon.vue";
+import ShareIcon from "@/components/icons/shareIcon.vue";
 
 const props = defineProps({
   image: String,
@@ -65,12 +71,12 @@ const props = defineProps({
   three: Boolean,
   date: String,
   blog_category: String,
-  blog: Object
-})
+  blog: Object,
+});
 
 const dateFormat = (date) => {
-  return dayjs(date).format('DD MMMM YYYY')
-}
+  return dayjs(date).format("DD MMMM YYYY");
+};
 </script>
 
 <style scoped></style>
---
+++ src/services/Skills.js
@@ -0,0 +1,21 @@
+import axios from '../axios'
+import { catchAxiosError } from './Response'
+
+export const getAllSkills = async () => {
+  try {
+    let res = await axios.get(`skills`)
+    return res.data
+  } catch (error) {
+    catchAxiosError(error)
+    throw error
+  }
+}
+export const getJobTitle = async () => {
+  try {
+    let res = await axios.get(`job-title`)
+    return res.data
+  } catch (error) {
+    catchAxiosError(error)
+    throw error
+  }
+}
