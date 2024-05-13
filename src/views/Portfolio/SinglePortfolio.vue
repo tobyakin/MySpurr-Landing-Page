567c164c0e01f100ee926550796c2bacbd028be8
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2024-05-13 14:21:47 +0100

    Case Study: Date and View Button

--- src/views/Portfolio/SinglePortfolio.vue
+++ src/views/Portfolio/SinglePortfolio.vue
@@ -14,6 +14,12 @@
           </p>
         </div>
         <div class="flex flex-row gap-2">
+          <p class="text-[#244034c5] text-[20.9px] font-Satoshi400">Date:</p>
+          <p class="text-[#244034] text-[20.9px] font-Satoshi500">
+            {{ portfolio.created_at }}
+          </p>
+        </div>
+        <div class="flex flex-row gap-2">
           <p class="text-[#244034c5] text-[20.9px] font-Satoshi400">Tags:</p>
           <div></div>
           <div class="flex flex-row gap-1">
@@ -81,7 +87,7 @@
           loading="lazy"
           :src="portfolio?.featured_image"
           alt="cover image"
-          class="rounded-[13.076px] h-[560px] w-full object-contain mx-auto"
+          class="rounded-[13.076px] h-[100%] w-full object-contain mx-auto"
         />
         <div class="flex flex-col gap-10">
           <img
@@ -90,8 +96,17 @@
             loading="lazy"
             :src="image.image"
             alt="cover image"
-            class="rounded-[13.076px] h-[560px] w-full object-contain mx-auto"
+            class="rounded-[13.076px] h-[100%] w-full object-contain mx-auto"
           />
+        </div>
+        <div class="pl-[2.53rem] ">
+          <a
+              role="button"
+              target="_blank"
+              href=""
+              class="btn-brand font-Satoshi500"
+              >View Full Project
+            </a>
         </div>
         <!-- <div class="flex flex-row justify-center gap-5 items-center">
           <button class="relative">
