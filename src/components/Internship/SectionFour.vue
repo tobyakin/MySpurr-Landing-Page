339486f1ea7e5c98b4577ef880a1d7814091737c
Salawudeen Abdultawab <abdultawabsalawudeen@gmail.com>
2024-11-30 10:25:49 +0100

    Internship Page

--- src/components/Internship/SectionFour.vue
+++ src/components/Internship/SectionFour.vue
@@ -1,11 +1,11 @@
 <template>
-  <section class="container lg:py-[145px] !lg:px-0 lg:mb-16">
+  <section class="container lg:py-[9.06rem] !lg:px-0 lg:mb-16">
     <div class="grid lg:grid-cols-2 grid-cols-1">
       <div class="">
         <img
           loading="lazy"
           class="h-[100%] w-[100%]"
-          src="@/assets/img/biglogo.png"
+          src="@/assets/img/bigLogoRight.png"
           alt=""
         />
       </div>
@@ -14,16 +14,16 @@
         class="lg:col-span-1 w-full flex flex-col items-center lg:px-8 justify-center px-4 py-12 lg:p-8"
       >
         <div class="flex-col flex gap-6">
-          <h4 class="font-Satoshi500 text-[#949939] text-[23.559px] !mb-3">
+          <h4 class="font-Satoshi500 text-[#949939] text-[1.47rem] !mb-3">
             FOR BUSINESSES
           </h4>
           <h4
-            class="text-[#007582] text-[45px] lg:text-[72px] font-EBGaramond500 leading-[40px] lg:leading-[67.5px]"
+            class="text-[#007582] text-[2.8rem] lg:text-[4.5rem] font-EBGaramond500 leading-[2.5rem] lg:leading-[4.22rem]"
           >
             Host Interns with MySpurr
           </h4>
           <p
-            class="text-[#000000B2] font-Satoshi400 text-[18px] lg:text-[24px] leading-[30px] lg:leading-[43px]"
+            class="text-[#000000B2] font-Satoshi400 text-[1.125rem] lg:text-[1.5rem] leading-[1.88rem] lg:leading-[2.69rem]"
           >
             Are you a business looking to tap into the fresh talent pool? MySpurr
             Internships provides a platform to connect with bright and motivated interns.
@@ -35,4 +35,3 @@
     </div>
   </section>
 </template>
-kk
--- src/components/Internship/SectionThree.vue
+++ src/components/Internship/SectionThree.vue
@@ -1,20 +1,20 @@
 <template>
   <section class="container !my-16 lg:!my-16">
     <h4
-      class="lg:text-[72px] text-[40px] font-EBGaramond500 leading-[40px] lg:leading-[67.5px] !mb-10"
+      class="lg:text-[4.5rem] text-[2.5rem] font-EBGaramond500 leading-[2.5rem] lg:leading-[4.22rem] !mb-10"
     >
       It’s easy to get started
     </h4>
-    <div class="grid container !px-0 lg:grid-cols-3 lg:gap-[70px] gap-[30px] grid-cols-1">
+    <div class="grid container !px-0 lg:grid-cols-3 lg:gap-[4.4rem] gap-[1.88rem] grid-cols-1">
       <div
         v-for="item in boxLists"
         :key="item"
         class="w-full bg-[#FBFBEE] flex flex-col text-center rounded-[4.71px] items-center lg:px-8 px-4 py-12 lg:p-8"
       >
-        <h4 class="text-[#007582] font-Satoshi500 text-[23px] leading-[24.15px] !mb-3">
+        <h4 class="text-[#007582] font-Satoshi500 text-[1.44rem] leading-[1.51rem] !mb-3">
           {{ item.heading }}
         </h4>
-        <p class="text-[#011B1F] text-[14px] font-Satoshi400 lg:leading-[16.87px]">
+        <p class="text-[#011B1F] text-[0.875rem] font-Satoshi400 lg:leading-[1.05rem]">
           {{ item.text }}
         </p>
       </div>
@@ -26,7 +26,7 @@ import { ref } from "vue";
 const boxLists = ref([
   {
     heading: " Signup",
-    text: `Create your MySpurr Internships profile to get started.`,
+    text: `Create your MySpurr profile as an  Intern to get started.`,
   },
   {
     heading: "Discover Internships",
@@ -34,19 +34,19 @@ const boxLists = ref([
   },
   {
     heading: "Apply",
-    text: `Submit your applications to the internships that align with your goals.`,
+    text: `Submit your applications to opportunities that align with your goals.`,
   },
   {
     heading: "Interview",
-    text: `Submit your applications to the internships that align with your goals.`,
+    text: `You may be called upon for an interview to discuss expectations.`,
   },
   {
     heading: "Intern",
-    text: `Start your internship journey, learn, and grow.`,
+    text: `Start your internship journey, learn, and grow your career journey with MySpurr.`,
   },
   {
     heading: "Succeed",
-    text: `Use this experience as a stepping stone for your future career.`,
+    text: `Use this experience as a stepping stone for future growth all within MySpurr.`,
   },
 ]);
 </script>
