47c79ef9fa43abee5bb94e4070a4454b498cf06c
onyiimesi <onyedikachukwu62@gmail.com>
2026-03-19 11:33:15 +0100

    Upgrade

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 import 'animate.css'
 import Toast, { POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
-import { createUnhead } from '@unhead/vue';
+import { createUnhead, VueUseHead } from '@unhead/vue'
 //import Vue3Autocounter from 'vue3-autocounter'
 import { VueQueryPlugin } from 'vue-query'
 import Antd from 'ant-design-vue'
@@ -17,11 +17,12 @@ import router from './router'
 const app = createApp(App)
 const head = createUnhead();
 
+app.use(VueUseHead, { head })
+
 app.use(Toast, { position: POSITION.TOP_RIGHT })
 //app.component('vue3-autocounter', Vue3Autocounter)
 app.use(VueQueryPlugin)
 app.use(createPinia())
 app.use(Antd)
 app.use(router)
-app.use(head);
 app.mount('#app')
